5971a64bad8e4ccb6a3ad9c673d2636aee81e3ef
Ben Simms <ben@bensimms.moe>
2018-12-09 06:10:46 +0000

    It compiles?

--- src/YAHDL/Gateway/Shard.hs
+++ src/YAHDL/Gateway/Shard.hs
@@ -8,6 +8,7 @@ module YAHDL.Gateway.Shard
   )
 where
 
+import Control.Monad.Log (Logger, runLogTSafe, askLogger)
 import           Control.Monad.Trans.Maybe
 import           Control.Monad.Trans.Except
 import           Control.Monad.State.Concurrent.Strict
@@ -31,19 +32,20 @@ newShardState :: Shard -> ShardState
 newShardState shard = ShardState shard Nothing Nothing Nothing False Nothing Nothing
 
 -- | Creates and launches a shard
-newShard :: Integer -> Text -> TChan () -> IO (Shard, Async ())
-newShard id token evtChan = mdo
+newShard :: Integer -> Text -> Logger env -> TChan () -> IO (Shard, Async ())
+newShard id token logEnv evtChan = mdo
   cmdChan' <- newTChanIO
   stateVar <- newTVarIO (newShardState shard)
 
   let shard = Shard id evtChan cmdChan' stateVar token
 
-  thread' <- async . runShardM shard $ shardLoop
+  thread' <- async . runShardM shard logEnv $ shardLoop
 
   pure (shard, thread')
 
-runShardM :: Shard -> ShardM a -> IO a
-runShardM shard action = evalStateC action (shard ^. shardState)
+runShardM :: Shard -> Logger env -> ShardM env a -> IO a
+runShardM shard logEnv action = evalStateC' . runLogTSafe logEnv . unShardM $ action
+  where evalStateC' s = evalStateC s (shard ^. shardState)
 
 extractMaybeStream :: (S.IsStream t, Monad m) => t m (Maybe a) -> t m a
 extractMaybeStream = S.mapMaybe identity
@@ -56,7 +58,7 @@ liftMaybe :: (MonadPlus m) => Maybe a -> m a
 liftMaybe = maybe mzero return
 
 -- | The loop a shard will run on
-shardLoop :: ShardM ()
+shardLoop :: ShardM env ()
 shardLoop = outerloop $> ()
  where
   controlStream shard = S.repeatM $ do
@@ -74,6 +76,7 @@ shardLoop = outerloop $> ()
   -- Currently if this goes to the error path we just exit the forever loop
   -- and the shard stops, maybe we might want to do some extra logic to reboot
   -- the shard, or maybe force a resharding
+  outerloop :: ShardM env (Either e a)
   outerloop = runExceptT . forever $ do
     state <- get
     shard <- use shardS
@@ -86,29 +89,35 @@ shardLoop = outerloop $> ()
         pure host
 
     -- TODO: handle result of innerloop
+    logEnv <- askLogger
+
     liftIO
-      $ runSecureClient (host ^. unpacked) 443 "/?v=7&encoding=json" (runShardM shard . innerloop)
+      $ runSecureClient (host ^. unpacked) 443 "/?v=7&encoding=json"
+                        (runShardM shard logEnv . innerloop)
 
   -- | The inner loop, handles receiving a message from discord or a command message
   -- | and then decides what to do with it
-  innerloop :: Connection -> ShardM ()
+  innerloop :: Connection -> ShardM env ()
   innerloop ws = do
     shard <- use shardS
     wsConn ?= ws
-    liftIO $ S.runStream $ mergedStream shard ws & S.mapM (atomically . handleMsg)
+    logEnv <- askLogger
+    liftIO $ S.runStream $ mergedStream shard ws & S.mapM (runShardM shard logEnv . handleMsg)
     wsConn .= Nothing
 
   -- | Handlers for each message, not sure what they'll need to do exactly yet
   handleMsg (Discord msg) = pure ()
   handleMsg (Control msg) = pure ()
 
-startHeartBeatLoop :: ShardM ()
+startHeartBeatLoop :: ShardM env ()
 startHeartBeatLoop = do
   haltHeartBeat -- cancel any currently running hb thread
   shard <- use shardS
-  liftIO $ finally (async . runShardM shard $ heartBeatLoop) (runShardM shard $ haltHeartBeat) $> ()
+  logEnv <- askLogger
+  liftIO $ finally (async . runShardM shard logEnv $ heartBeatLoop)
+                   (runShardM shard logEnv haltHeartBeat) $> ()
 
-haltHeartBeat :: ShardM ()
+haltHeartBeat :: ShardM env ()
 haltHeartBeat = do
   thread <- use hbThread
   case thread of
@@ -116,10 +125,10 @@ haltHeartBeat = do
     Nothing -> pure ()
   hbThread .= Nothing
 
-sendHeartBeat :: ShardM ()
+sendHeartBeat :: ShardM env ()
 sendHeartBeat = pure ()
 
-heartBeatLoop :: ShardM ()
+heartBeatLoop :: ShardM env ()
 heartBeatLoop = ($> ()) . liftIO . runMaybeT . forever $ do
   pure ()
   pure ()
--- src/YAHDL/Gateway/Types.hs
+++ src/YAHDL/Gateway/Types.hs
@@ -8,11 +8,13 @@ module YAHDL.Gateway.Types where
   -- , ShardM(..)
   -- , ShardMsg(..)
   -- , DiscordMessage(..)
-  -- , RawDiscordMessage(..)
+  -- , RawWsPayload(..)
   -- , ControlMessage(..)
   -- )
 
-import           Control.Lens                   ( makeLenses )
+import Control.Monad.Log (LogT, MonadLog)
+import  GHC.Generics
+import           Control.Lens                   ( makeFields )
 import           Data.Aeson
 import           Control.Concurrent.STM.TChan
 import           Control.Concurrent.STM.TVar
@@ -20,42 +22,158 @@ import           Control.Concurrent.STM.TMVar
 import           Control.Monad.State.Concurrent.Strict
 import           Network.WebSockets.Connection  ( Connection )
 
--- TODO: change this from RawDiscordMessage to DiscordMessage, add decoder & handler
-data ShardMsg = Discord RawDiscordMessage | Control ControlMessage
+-- TODO: change this from RawWsPayload to DiscordMessage, add decoder & handler
+data ShardMsg = Discord RawWsPayload | Control ControlMessage
 
--- TODO: this
-data DiscordMessage = DEvent
+data RawWsPayload = RawWsPayload
+  { _op :: Integer
+  , _d  :: Maybe Value
+  , _s  :: Maybe Int
+  , _t  :: Maybe Text
+  } deriving (Show, Generic)
+
+instance ToJSON RawWsPayload
+instance FromJSON RawWsPayload
+
+data DiscordMessage
+  = Dispatch DispatchData
+  | HeartBeat Integer
+  | Identify IdentifyData
+  | StatusUpdate StatusUpdateData
+  | VoiceStatusUpdate
+  | VoiceServerPing
+  | Resume
+  | Reconnect
+  | RequestGuildMembers
+  | InvalidSession
+  | Hello
+  | HeartBeatAck
   deriving (Show, Generic)
 
-data RawDiscordMessage = RDEvent
-  deriving (Show, Generic)
+instance ToJSON DiscordMessage
+instance FromJSON DiscordMessage
 
-instance ToJSON RawDiscordMessage
-instance FromJSON RawDiscordMessage
+-- Thanks sbrg:
+-- https://github.com/saevarb/haskord/blob/d1bb07bcc4f3dbc29f2dfd3351ff9f16fc100c07/haskord-lib/src/Haskord/Types/Common.hs
+data EventType
+    = READY
+    | CHANNEL_CREATE
+    | CHANNEL_UPDATE
+    | CHANNEL_DELETE
+    | CHANNEL_PINS_UPDATE
+    | GUILD_CREATE
+    | GUILD_UPDATE
+    | GUILD_DELETE
+    | GUILD_BAN_ADD
+    | GUILD_BAN_REMOVE
+    | GUILD_EMOJIS_UPDATE
+    | GUILD_INTEGRATIONS_UPDATE
+    | GUILD_MEMBER_ADD
+    | GUILD_MEMBER_REMOVE
+    | GUILD_MEMBER_UPDATE
+    | GUILD_MEMBERS_CHUNK
+    | GUILD_ROLE_CREATE
+    | GUILD_ROLE_UPDATE
+    | GUILD_ROLE_DELETE
+    | MESSAGE_CREATE
+    | MESSAGE_UPDATE
+    | MESSAGE_DELETE
+    | MESSAGE_DELETE_BULK
+    | MESSAGE_REACTION_ADD
+    | MESSAGE_REACTION_REMOVE
+    | MESSAGE_REACTION_REMOVE_ALL
+    | PRESENCE_UPDATE
+    | TYPING_START
+    | USER_UPDATE
+    | VOICE_STATE_UPDATE
+    | VOICE_SERVER_UPDATE
+    | WEBHOOKS_UPDATE
+    deriving (Show, Eq, Generic)
 
-data ControlMessage = Restart
+instance ToJSON EventType
+instance FromJSON EventType
+
+data DispatchData = DispatchData
+  { _dispatchDataData :: Value
+  , _dispatchDataName :: EventType
+  } deriving (Show, Generic)
+
+instance ToJSON DispatchData
+instance FromJSON DispatchData
+
+data IdentifyData = IdentifyData
+  { _identifyDataToken :: Text
+  , _identifyDataProperties :: IdentifyProps
+  , _identifyDataCompress :: Maybe Bool
+  , _identifyDataLarge_threshold :: Maybe Int
+  , _identifyDataShard :: Maybe Int
+  , _identifyDataPresence :: Maybe PresenceData
+  } deriving (Show, Generic)
+
+instance ToJSON IdentifyData
+instance FromJSON IdentifyData
+
+data StatusUpdateData = StatusUpdateData
+  { _statusUpdateDataSice :: Maybe Int
+  , _statusUpdateDataGame :: Maybe Value -- TODO: activity object
+  , _statusUpdateDataStatus :: Text
+  , _statusUpdateDataAfk :: Bool
+  } deriving (Show, Generic)
+
+instance ToJSON StatusUpdateData
+instance FromJSON StatusUpdateData
+
+-- TODO(ben): Work on filling out data types and aeson stuff
+
+data IdentifyProps = IdentifyProps
+  { _identifyPropsOs :: Text
+  , _identifyPropsBrowser :: Text
+  , _identifyPropsDevice :: Text
+  } deriving (Show, Generic)
+
+instance ToJSON IdentifyProps
+instance FromJSON IdentifyProps
+
+data PresenceData = PresenceData
+  { _presenceDataSince :: Maybe Int
+  , _presenceDataGame :: Maybe Value -- TODO: activity object
+  , _presenceDataStatus :: Text
+  , _presenceDataAfk :: Bool
+  } deriving (Show, Generic)
+
+instance ToJSON PresenceData
+instance FromJSON PresenceData
+
+data ControlMessage = Restart | ShutDown
   deriving (Show)
 
 data Shard = Shard
-  { _shardId :: Integer
-  , _evtChan :: TChan () -- TODO: replace this with the event type
-  , _cmdChan :: TChan ControlMessage -- TODO: replace this with the shard command type
-  , _shardState :: TVar ShardState
-  , _token :: Text
+  { _shardShardId :: Integer
+  , _shardEvtChan :: TChan () -- TODO: replace this with the event type
+  , _shardCmdChan :: TChan ControlMessage -- TODO: replace this with the shard command type
+  , _shardShardState :: TVar ShardState
+  , _shardToken :: Text
   } deriving (Generic)
 
 data ShardState = ShardState
-  { _shardS :: Shard
-  , _seqNum :: Maybe Integer
-  , _hbThread :: Maybe (Async ())
-  , _wsHost :: Maybe Text
-  , _wsResponse :: Bool
-  , _sessionID :: Maybe Integer
-  , _wsConn :: Maybe Connection
+  { _shardStateShardS :: Shard
+  , _shardStateSeqNum :: Maybe Integer
+  , _shardStateHbThread :: Maybe (Async ())
+  , _shardStateWsHost :: Maybe Text
+  , _shardStateWsResponse :: Bool
+  , _shardStateSessionID :: Maybe Integer
+  , _shardStateWsConn :: Maybe Connection
   } deriving (Generic)
 
-makeLenses ''Shard
-makeLenses ''ShardState
+makeFields ''Shard
+makeFields ''ShardState
 
-type ShardM a = StateC ShardState IO a
+newtype ShardM env a = ShardM
+  { unShardM :: LogT env (StateC ShardState IO) a
+  } deriving (Applicative, Monad, MonadIO, MonadLog env,
+              Functor, MonadState ShardState)
 
+instance MonadState s m => MonadState s (LogT env m) where
+  get = lift get
+  put = lift . put
+  state = lift . state
