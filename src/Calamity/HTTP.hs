8c9d515a80c9896e74f3723cbc65e14f85c78a16
Ben Simms <ben@bensimms.moe>
2020-05-03 02:16:38 +0100

    Add Invite endpoints

--- src/Calamity/HTTP.hs
+++ src/Calamity/HTTP.hs
@@ -4,10 +4,12 @@ module Calamity.HTTP
     , module Calamity.HTTP.Channel
     , module Calamity.HTTP.Emoji
     , module Calamity.HTTP.Guild
+    , module Calamity.HTTP.Invite
     , module Calamity.HTTP.MiscRoutes ) where
 
 import           Calamity.HTTP.Channel
 import           Calamity.HTTP.Emoji
 import           Calamity.HTTP.Guild
 import           Calamity.HTTP.Internal.Request ( invokeRequest )
+import           Calamity.HTTP.Invite
 import           Calamity.HTTP.MiscRoutes
---
+++ src/Calamity/HTTP/Invite.hs
@@ -0,0 +1,31 @@
+-- | Invite endpoints
+module Calamity.HTTP.Invite
+    ( InviteRequest(..) ) where
+
+import           Calamity.HTTP.Internal.Request
+import           Calamity.HTTP.Internal.Route
+import           Calamity.Types.Model.Guild
+
+import           Control.Lens                   hiding ( (.=) )
+
+import           Data.Text                      ( Text )
+
+import           Network.Wreq
+
+import           TextShow
+
+data InviteRequest a where
+  GetInvite :: Text -> InviteRequest Invite
+  DeleteInvite :: Text -> InviteRequest ()
+
+baseRoute :: RouteBuilder _
+baseRoute = mkRouteBuilder // S "invites"
+
+instance Request (InviteRequest a) a where
+  toRoute (GetInvite c) = baseRoute // S c
+    & buildRoute
+  toRoute (DeleteInvite c) = baseRoute // S c
+    & buildRoute
+
+  toAction (GetInvite _) = getWithP (param "with_counts" .~ [showt True])
+  toAction (DeleteInvite _) = deleteWith
