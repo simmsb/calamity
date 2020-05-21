ed0daa3ae0ecb7a3112eac139062becf64af658b
Ben Simms <ben@bensimms.moe>
2020-05-21 04:03:07 +0100

    Add commands module

---
+++ src/Calamity/Commands.hs
@@ -0,0 +1,21 @@
+-- | Calamity commands
+module Calamity.Commands
+    ( module Calamity.Commands.Check
+    , module Calamity.Commands.Command
+    , module Calamity.Commands.Context
+    , module Calamity.Commands.Dsl
+    , module Calamity.Commands.Error
+    , module Calamity.Commands.Group
+    , module Calamity.Commands.Handler
+    , module Calamity.Commands.ParsePrefix
+    , module Calamity.Commands.Parser ) where
+
+import           Calamity.Commands.Check
+import           Calamity.Commands.Command
+import           Calamity.Commands.Context
+import           Calamity.Commands.Dsl
+import           Calamity.Commands.Error
+import           Calamity.Commands.Group
+import           Calamity.Commands.Handler
+import           Calamity.Commands.ParsePrefix
+import           Calamity.Commands.Parser
